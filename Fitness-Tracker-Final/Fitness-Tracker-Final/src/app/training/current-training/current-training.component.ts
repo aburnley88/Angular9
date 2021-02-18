ae3943f6ef1ae542e19d9244bb76c3efe8f9733a
Arnold Burnley <aburnleylvx@outlook.com>
2021-02-18 10:54:43 -0500

    added exit and continue logic for progress

--- Fitness-Tracker-Final/Fitness-Tracker-Final/src/app/training/current-training/current-training.component.ts
+++ Fitness-Tracker-Final/Fitness-Tracker-Final/src/app/training/current-training/current-training.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './../stop-training/stop-training.component';
 
@@ -9,22 +9,38 @@ import { StopTrainingComponent } from './../stop-training/stop-training.componen
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: any; 
 
   constructor(private dialog: MatDialog ) { }
 
   ngOnInit(): void {
+    this.startOrResumeTraining();     
+  }
+
+  startOrResumeTraining() {
     this.timer =setInterval(() => {
       this.progress = this.progress + 5;
       if(this.progress >= 100) {
         clearInterval(this.timer);
       }
-    }, 1000);   
+    }, 1000); 
   }
+
   onStop() {
     clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {data: {
+      progress: this.progress
+    }});
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTraining();
+      }
+    });
   }
 
 }
